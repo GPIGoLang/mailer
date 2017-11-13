71dd23dc09efdf4843de7a6b7a56942a793c8c68
Emmanuel Benson <emmanuel.benson@my-gpi.com>
2017-11-13 16:34:15 +0100

    Server configurations

---
+++ Server.go
@@ -0,0 +1,14 @@
+package mailer
+
+const (
+	// HOST constant
+	HOST = "vps28383.inmotionhosting.com"
+	// PORT constant
+	PORT = "465"
+	// FROM constant
+	FROM = "noreply@mipad.my-gpi.org"
+	// PASSWORD constant
+	PASSWORD = "gPIInoReply{)}(!@"
+	// SERVERNAME constant
+	SERVERNAME = HOST + ":" + PORT
+)
